b9efc4495d7c6d1877eacb15f3cccf117ba01a0f
François Georgy <francois.georgy@gmail.com>
2018-04-17 16:51:59 +0200

    migration to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,42 @@
 const webpack = require("webpack");
-const path = require('path')
-// const glob = require('glob');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: {
-        bundle: './src/main.js',
-        print_bundle: './src/print.js'
-    },
-    output: {
-        filename: './[name].js',
-        path: path.resolve(__dirname, 'dist') //,
+        bundle: "./src/main.js",
+        print_bundle: "./src/print.js"
     },
     module: {
         rules: [{
                  test: /\.css$/,
-                 use: ['style-loader', 'css-loader']
+                 use: ["style-loader", "css-loader"]
         }]
     },
     plugins: [
-        // new CleanWebpackPlugin(['dist']),
-        new MinifyPlugin(/*minifyOpts*/ {
-            removeConsole: true,
-            removeDebugger: true
-        }, /*pluginOpts*/ {}),
+        // new CleanWebpackPlugin(["dist"]),
+        // new MinifyPlugin(/*minifyOpts*/ {
+        //     removeConsole: true,
+        //     removeDebugger: true
+        // }, /*pluginOpts*/ {}),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            "window.jQuery": "jquery'",
+            "window.jQuery": "jquery",
             "window.$": "jquery"
         }),
         new CopyWebpackPlugin([
-            { from: './src/index.html' },
-            { from: './src/midi.html' },
-            { from: './src/print.html' },
-            { from: './src/templates/patch-sheet-template.html', to: 'templates'},
-            { from: './src/css/midi.css', to: 'css' },
-            { from: './src/favicon.png' }
-            // { from: './src/css/patch.css', to: 'css' },
-            // { from: './src/css/print.css', to: 'css' },
+            { from: "./src/index.html" },
+            { from: "./src/midi.html" },
+            { from: "./src/print.html" },
+            { from: "./src/templates/patch-sheet-template.html", to: "templates"},
+            { from: "./src/css/midi.css", to: "css" },
+            { from: "./src/favicon.png" }
+            // { from: "./src/css/patch.css", to: "css" },
+            // { from: "./src/css/print.css", to: "css" },
         ])
-    ]
+    ],
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    }
 };
